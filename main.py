e27e6b4ab2b682bebf38201f32eb74e541c82484
Pablo Cusnir <pabloc@Pablos-MacBook-Pro.local>
2022-11-14 19:37:35 +0200

    Initial commit and push

---
+++ main.py
@@ -0,0 +1,18 @@
+
+"""
+TODO -
+0. Create default, config and password files. TOML?
+1. GET something from given address
+2. Read and write to file
+3. retry - see https://github.com/invl/retry
+
+
+"""
+
+
+def hello_world(name: str = 'John Doe'):
+    print(f'Hello world!\nHello {name}!')
+
+
+if __name__ == '__main__':
+    hello_world()
